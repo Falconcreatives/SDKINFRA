54f2edada98292499643fd52953d710db3412cd5
Falconcreatives <falconcreatives@gmail.com>
2025-02-05 00:05:56 +0530

    SDK

---
+++ src/pages/contactus.jsx
@@ -0,0 +1,15 @@
+import Footer from "../componants/Footer";
+import Contactusbanner from "../componants/Contactusbanner"; 
+import Contactus1 from "../componants/Contactus1";
+
+function Contactus() {
+    return (
+        <div>
+            <Contactusbanner/>
+            <Contactus1/>
+            <Footer />
+        </div>
+    );
+}
+
+export default Contactus;
---
+++ src/pages/quality.jsx
@@ -0,0 +1,16 @@
+import Footer from "../componants/Footer";
+import Quality1 from "../componants/Quality1";
+import Qualitybanner from "../componants/Qualitybanner";
+
+
+function Quality() {
+    return (
+        <div>
+           <Qualitybanner/>
+           <Quality1/>
+           <Footer/>
+        </div>
+    );
+}
+
+export default Quality;
